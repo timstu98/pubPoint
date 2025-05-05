ba66c48a1dc301c6e81c785287bc49bdfef1f1b0
Timothy Stuart <timstu98@gmail.com>
2025-05-05 20:28:16 +0100

    tidy: Removed boilerplate react

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -1,79 +1,13 @@
-// import { useState } from 'react';
-// import { MapContainer, TileLayer, useMap } from 'react-leaflet';
 import Navbar from './components/NavBar';
-import Hero from './components/Hero';
-import HomeCards from './components/HomeCards';
-import JobListings from './components/JobListings';
-
+import MapDisplay from './components/MapDisplay';
 
 const App = () => {
   return (
     <>
-    <Navbar />
-    <Hero/>
-    <HomeCards />
-    <JobListings/>
-    
-
-    <section className="m-auto max-w-lg my-10 px-6">
-      <a
-        href="jobs.html"
-        className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >View All Jobs</a
-      >
-    </section>
+      <Navbar />
+      <MapDisplay />
     </>
-  )
-}
+  );
+};
 
-export default App
-{/* 
-function App() {
-  const [count, setCount] = useState(0)
-
-  var map = L.map('map').setView([51.505, -0.09], 13);
-  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
-
-  return (
-    <>
-      <link
-        rel="stylesheet"
-        href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"
-        integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
-        crossorigin=""
-      />
-      <MapContainer center={[45.4, -75.7]} zoom={12}scrollWheelZoom={false}>
-        <TileLayer
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        />
-      </MapContainer>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} classNameName="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} classNameName="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div classNameName="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p classNameName="read-the-docs">
-        Click on the Vite and React logos to learn more
-        <div id="map"></div>
-      </p>
-    </>
-  )
-}
-
-export default App */}
+export default App;
